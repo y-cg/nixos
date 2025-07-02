accb37755efafa0cef56d5c91a428f17a2a409d6
y-cg <159802291+y-cg@users.noreply.github.com>
2025-07-02 20:47:28 +0800

    fix: remove linuxPackages_rpi4 due to the error

--- specific/rpi4/boot.nix
+++ specific/rpi4/boot.nix
@@ -1,7 +1,11 @@
 { pkgs, lib, ... }:
 {
   boot.loader.grub.enable = false;
-  boot.kernelPackages = pkgs.linuxPackages_rpi4;
+
+  # Disable due to the evaluation error:
+  # See: https://github.com/NixOS/nixpkgs/issues/154163
+  # boot.kernelPackages = pkgs.linuxPackages_rpi4;
+
   # This fix the problem that rpi fail to reconnect to wifi after reboot in a fresh nixos-rebuild
   # See https://github.com/Robertof/nixos-docker-sd-image-builder/issues/10#issuecomment-646901392
   hardware.enableRedistributableFirmware = true; # Includes wifi kernel modules.
