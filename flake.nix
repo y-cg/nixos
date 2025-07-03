fe8b94cbda04dd6fe28ca16691cc1cb43a7ac157
y-cg <159802291+y-cg@users.noreply.github.com>
2025-07-04 00:47:34 +0800

    fix: pin home-manager and nixpkgs to same version

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "YCG's NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
