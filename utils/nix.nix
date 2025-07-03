6d8f803f77a7eb9965a4a3b86e44eccb209d81da
y-cg <159802291+y-cg@users.noreply.github.com>
2025-07-03 20:11:09 +0800

    feat: make sure I am in the trusted users list

--- utils/nix.nix
+++ utils/nix.nix
@@ -2,9 +2,15 @@
   # Allow unfree packages (some firmware might need this)
   nixpkgs.config.allowUnfree = true;
 
-  # Enable flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      # Enable flakes
+      "flakes"
+    ];
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
 }
