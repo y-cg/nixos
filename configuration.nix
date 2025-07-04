68dec5f87c2c13deee4c00b34823d7faf81bff59
y-cg <159802291+y-cg@users.noreply.github.com>
2025-07-04 19:58:48 +0800

    fix: pin system.stateVersion to matched one

--- configuration.nix
+++ configuration.nix
@@ -27,5 +27,5 @@
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken.
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 }
