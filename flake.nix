c4a03aadb9632b9665ae3d0b73c0925a5e86fd78
y-cg <159802291+y-cg@users.noreply.github.com>
2026-01-29 01:40:45 +0800

    chore: bump 25.05 to 25.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "YCG's NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
